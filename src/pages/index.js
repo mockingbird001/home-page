c897277c0ad506c8f4fe2ece755b333c8bc2b852
mockingbird001 <bom_bom0123456789@hotmail.com>
2021-04-26 16:12:38 +0700

    navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "../component/Navbar";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Navbar />
     </div>
   );
 };
